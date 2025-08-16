ddb24eaa62e8839881dd0722947470753d710c29
matheusmen1 <antonuniccimatheus1@outlook.com>
2025-08-16 10:46:28 -0300

    Finalizando matriz adjacencia Leitura por arq txt Exibir Matriz Com as Vertices Verifica Grafo ou Digrafo Verifica Simples Verfica Regular Verificar Completo

--- TrabalhoTGNovo.cpp
+++ TrabalhoTGNovo.cpp
@@ -2,12 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <conio2.h>
 
-#define L 4
-#define C 4
+#define L 50
+#define C 50
 
-// funcoes
-void verificarOrientadoAdjacencia(int m[L][C], int *grafo)
+void lerTxTMatrizAdjacencia(int m[L][C], int *qtdeVertices, int *qtdeLinhas, int *qtdeColunas, char vertices[C])
+{
+
+	int j = 0, num, linhas = 0, colunas = 0;
+	char linha[50], aux[50];
+	FILE * Ptr = fopen("entradaMA.txt", "r");
+	rewind(Ptr); // joga o ponteiro para o inicio
+	fgets(linha, sizeof(linha), Ptr); // le a primeira linha, contendo todos meus vertices
+	for (int i = 0; i < strlen(linha);i++)
+	{
+		if (linha[i] != ' ' && linha[i] != '\n')
+		{
+			vertices[j] = linha[i];
+			*qtdeVertices = *qtdeVertices + 1;
+			j++;
+		}
+		//printf("%c", linha[i]);
+	}
+	// ler a matriz
+	
+	while(fgets(linha, sizeof(linha), Ptr))
+	{
+		j = 0;
+		colunas = 0;
+		for(int i = 0; i < strlen(linha); i++)
+		{
+			if(linha[i] != ' ' && linha[i] != '\n')
+			{
+				aux[j] = linha[i];
+				j++;
+			}
+			else
+			{
+				aux[j] = '\0';
+				num = atoi(aux);
+				m[linhas][colunas] = num;
+				j = 0;
+				colunas++;
+			}
+		
+			
+		}
+		if (j > 0)
+		{
+			
+			aux[j] = '\0';
+			num = atoi(aux);
+			m[linhas][colunas] = num;
+
+		}
+		if (colunas > *qtdeColunas)
+			*qtdeColunas = colunas;
+		linhas++;
+	}
+	*qtdeLinhas = linhas;
+	
+	fclose(Ptr);
+}
+void verificarOrientadoAdjacencia(int m[L][C], int l, int c,int *grafo)
 {
 	for (int i = 0; i < L; i++)
 	{
@@ -15,31 +73,34 @@ void verificarOrientadoAdjacencia(int m[L][C], int *grafo)
 		{
 			if (m[i][j] != m[j][i]) // orientado (nao simetrico)
 				*grafo = 1;
+		
 		}
+		
 	}
+	
 }
-void verificaSimplesAdjacencia(int m[L][C], int *simples)
+void verificaSimplesAdjacencia(int m[L][C],int l, int c, int *simples)
 {
-	*simples = 0;
-	for (int i = 0; i < L; i++)
+	for (int i = 0; i < l; i++)
 	{
-		for (int j = 0; j < C; j++)
+		for (int j = 0; j < c; j++)
 		{
-			if (j == i && m[i][j] != 0)
+			if (j == i && m[i][j] != 0)			
 				*simples = 1;
 		}
 	}
+	
 }
-void verificarRegularAdjacencia(int m[L][C], int *regular)
+void verificarRegularAdjacencia(int m[L][C], int l, int c,int *regular)
 {
 	int cont, aux = 0;
-	for (int i = 0; i < L; i++)
+	for (int i = 0; i < l; i++)
 	{
 		cont = 0;
-		for (int j = 0; j < C; j++)
+		for (int j = 0; j < c; j++)
 		{
 			if (m[i][j] != 0)
-				cont++;
+				cont++;		
 		}
 		if (aux == 0) // primeira linha
 		{
@@ -49,156 +110,152 @@ void verificarRegularAdjacencia(int m[L][C], int *regular)
 		{
 			if (cont != aux) // nem todas as vertices sao regulares
 				*regular = 1;
+			
 		}
 	}
+	
 }
-void verificarCompletoAdjacencia(int m[L][C], int *completo, int v)
+void verificarCompletoAdjacencia(int m[L][C], int l, int c, int *completo, int v)
 {
 	int cont;
-	for (int i = 0; i < L; i++)
+	for (int i = 0; i < l; i++)
 	{
 		cont = 0;
-		for (int j = 0; j < C; j++)
+		for (int j = 0; j < c; j++)
 		{
 			cont++;
+			
 		}
-		if (cont != v - 1)
+		if (cont != v - 1) 
 			*completo = 1;
 	}
+	
 }
-void exibirMatriz(int m[L][C])
+void exibirVerticesAdjacencia(char vertices[C])
 {
-	for (int i = 0; i < L; i++)
+	for(int i = 0; i < strlen(vertices); i++)
+		printf("%c ", vertices[i]);
+	
+}
+void exibirMatrizAdjacencia(int m[L][C], int qtdeLinhas, int qtdeColunas)
+{
+	for (int i = 0; i < qtdeLinhas; i++)
 	{
-		for (int j = 0; j < C; j++)
+		for (int j = 0; j < qtdeColunas; j++)
 		{
-			printf("%d ", m[i][j]);
+			printf ("%d ", m[i][j]);
 		}
 		printf("\n");
 	}
+	
 }
-char menu(void)
+char menu (void)
 {
 	printf("\n[A] - Matriz de Adjacencia\n");
 	printf("[B] - Matriz de Incidencia\n");
-	printf("[ESC] - Sair\n");
-
-	return toupper(getchar());
+	printf ("[ESC] - Sair\n");
+	
+	return toupper(getch());
 }
 char submenu(void)
 {
 	printf("\n[A] - Simples\n");
 	printf("[B] - Regular\n");
 	printf("[C] - Completo\n");
-	printf("[ESC] - Voltar\n");
-
-	return toupper(getchar());
+	printf ("[ESC] - Voltar\n");
+	
+	return toupper(getch());
 }
 void executar(void)
 {
-	int m[L][C];
-	m[0][0] = 0;
-	m[0][1] = 1;
-	m[0][2] = 1;
-	m[0][3] = 0;
 
-	m[1][0] = 0;
-	m[1][1] = 0;
-	m[1][2] = 0;
-	m[1][3] = 1;
-
-	m[2][0] = 1;
-	m[2][1] = 0;
-	m[2][2] = 0;
-	m[2][3] = 1;
-
-	m[3][0] = 0;
-	m[3][1] = 1;
-	m[3][2] = 1;
-	m[3][3] = 0;
 	char op, op2;
 	do
-	{
+	{	
+		
 		int grafo = 0, simples = 0, regular = 0, completo;
-		// clrscr();
-		exibirMatriz(m);
-		verificarOrientadoAdjacencia(m, &grafo);
-		if (grafo == 1)
-			printf("\nGrafo Orientado (Digrafo)\n");
-		else
-			printf("\nGrafo Nao Orientado (Grafo)\n");
+		int m[L][C], qtdeVertices = 0, qtdeLinhas = 0, qtdeColunas = 0;
+		char vertices[C];
+
+		clrscr();
 		op = menu();
-		switch (op)
+		switch(op)
 		{
-		case 'A':
-			do
-			{
-				// clrscr();
-				exibirMatriz(m);
-				verificarOrientadoAdjacencia(m, &grafo);
-				if (grafo == 1)
-					printf("\nGrafo Orientado (Digrafo)\n");
-				else
-					printf("\nGrafo Nao Orientado (Grafo)\n");
-				op2 = submenu();
-				switch (op2)
+			case 'A':
+				lerTxTMatrizAdjacencia(m, &qtdeVertices, &qtdeLinhas, &qtdeColunas, vertices);
+				do
 				{
-
-				case 'A':
-					verificaSimplesAdjacencia(m, &simples);
-					if (simples == 1)
-						printf("\nNao eh simples");
+					clrscr();
+					exibirVerticesAdjacencia(vertices);
+					printf("\n");
+					exibirMatrizAdjacencia(m, qtdeLinhas,qtdeColunas);
+					verificarOrientadoAdjacencia(m, qtdeLinhas, qtdeColunas ,&grafo);
+					if (grafo == 1)
+						printf ("\nGrafo Orientado (Digrafo)\n");
 					else
-						printf("\nEh simples");
-
-					getchar();
-					break;
-				case 'B':
-					verificarRegularAdjacencia(m, &regular);
-					if (regular == 1)
-						printf("\nNao Regular\n");
-					else
-						printf("\nRegular\n");
-					getchar();
-					break;
-				case 'C':
-					if (regular == 0 && simples == 0) // tem q ser regular e simples, para ser completo
+						printf ("\nGrafo Nao Orientado (Grafo)\n");
+					op2 = submenu();
+					switch(op2)
 					{
-						verificarCompletoAdjacencia(m, &completo, 4);
-						if (completo == 1) // nao eh completo
-							printf("\nNao eh completo\n");
+					
+						case 'A':
+						verificaSimplesAdjacencia(m, qtdeLinhas, qtdeColunas,&simples);
+						if (simples == 1)
+							printf("\nNao eh simples");
 						else
-							printf("\nCompleto\n");
+							printf("\nEh simples");
+							
+						getch();
+						break;
+						case 'B':
+						verificarRegularAdjacencia(m, qtdeLinhas, qtdeColunas, &regular);
+						if (regular == 1)
+							printf ("\nNao Regular\n");
+						else
+							printf ("\nRegular\n");
+						getch();	
+						break;
+						case 'C':
+						if (regular == 0 && simples == 0) // tem q ser regular e simples, para ser completo
+						{
+							verificarCompletoAdjacencia(m, qtdeLinhas, qtdeColunas, &completo, qtdeVertices);
+							if (completo == 1) // nao eh completo
+								printf ("\nNao eh completo\n");
+							else
+								printf ("\nCompleto\n");
+						}
+						else
+							printf ("\nNao eh completo\n");
+						
+					
+						getch();
+						break;
+						
 					}
-					else
-						printf("\nNao eh completo\n");
-
-					getchar();
-					break;
-				}
-			} while (op2 != 27);
+				}while(op2 != 27);
 			break;
-		case 'B':
-			do
-			{
-				// clrscr();
-				op2 = submenu();
-				switch (op2)
-				{
-				case 'A':
-					printf("grafo orientado incidencia\n");
-					getchar();
-					break;
-				}
-			} while (op2 != 27);
+			case 'B':
+				do
+				{	clrscr();
+					op2 = submenu();
+					switch(op2)
+					{
+						case 'A':
+						printf("grafo orientado incidencia\n");
+						getch();
+						break;
+					}
+				}while(op2 != 27);
 			break;
+		
 		}
-
-	} while (op != 27);
+		
+	}while(op != 27);
+	
+	
 }
 int main(void)
 {
-
 	executar();
 	return 0;
 }
